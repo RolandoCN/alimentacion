c7a04c1cc657fec5d8617449046f0dfb58cb4573
RolandoCN <juan.roland@outlook.es>
2023-08-04 13:54:08 -0500

    valida hora igual en confirmacion2

--- app/Http/Controllers/Alimentacion/VerificaTurnoController.php
+++ app/Http/Controllers/Alimentacion/VerificaTurnoController.php
@@ -63,7 +63,8 @@ class VerificaTurnoController extends Controller
         ->where('tc.confirma_empleado','=','Si') //agg ultimos consideraciones  
         ->where('e.cedula',$cedula)      
         ->select('e.cedula', 'e.nombres', 'pu.nombre as puesto','a.nombre as area','h.hora_ini as hora_ini', 'h.hora_fin as hora_fin', 'tu.id as idturno','tu.start as fecha_turno', 'al.descripcion as comida', 'tc.estado as estado_turno','tc.id_turno_comida as id_turno_comida',
-        'tc.hora_retira_comida','tc.estado_retira_comida','tc.confirma_empleado','tc.fecha_elimina as fecha_eli')
+        'tc.hora_retira_comida','tc.estado_retira_comida','tc.confirma_empleado','tc.fecha_elimina as fecha_eli','tc.id_turno')
+        ->orderBy('id_turno', 'desc') //ultimo registro (x si eliminan y vuelven a ingresar y aprobar)
         ->first(); 
         
         if(is_null($turnos_aprobado)){
