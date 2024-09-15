018c712a2400b299bd5ebe676281ec808bf6d322
RolandoCN <juan.roland@outlook.es>
2024-09-15 08:05:25 -0500

    set time ali pac

--- app/Http/Controllers/Alimentacion/AlimentosPacientesController.php
+++ app/Http/Controllers/Alimentacion/AlimentosPacientesController.php
@@ -581,6 +581,9 @@ class AlimentosPacientesController extends Controller
 
     public function reportePdfAliServicio($serv){
         try{
+            set_time_limit(0);
+            ini_set("memory_limit",-1);
+            ini_set('max_execution_time', 0);
           
             $consultaPendiente=$this->listarVisor($serv);
             if($consultaPendiente['error']==true){
